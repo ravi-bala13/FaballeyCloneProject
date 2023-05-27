0636f0a0bfe67733867ed770fc1119a90a61d77f
Bala Murugan <bals161616@gmail.com>
2023-05-27 21:34:00 +0530

    using deployed link for backend

--- client/src/utils/Constants.js
+++ client/src/utils/Constants.js
@@ -1,6 +1,6 @@
 export const loadingGif =
   "https://media.tenor.com/guhB4PpjrmUAAAAM/loading-loading-gif.gif";
 
-// export const backendUrl = "https://faballeyclonebackend.onrender.com";
+export const backendUrl = "https://faballeyclonebackend.onrender.com";
 
-export const backendUrl = "http://localhost:2345";
+// export const backendUrl = "http://localhost:2345";
